fcef10eda2432be96e0f23fdf61bfd0ca34658af
Roman Danilov <danromannka96@gmail.com>
2018-02-11 16:04:18 +0500

    project

---
+++ Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace TPR1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button3 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Количество состояний";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Количество стратегий";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Кол-во шагов модел-ия";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(143, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(143, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(143, 83);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(589, 474);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Вывод матрицы";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(150, 111);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Итоговая таблица";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(16, 141);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(598, 500);
+            this.tabControl1.TabIndex = 10;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(590, 474);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Матрица";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(590, 474);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Итоговая таблица";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(2, 2);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(586, 474);
+            this.dataGridView2.TabIndex = 7;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.pictureBox1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(590, 474);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Граф";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(590, 474);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(629, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сохранитьToolStripMenuItem,
+            this.загрузитьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // загрузитьToolStripMenuItem
+            // 
+            this.загрузитьToolStripMenuItem.Name = "загрузитьToolStripMenuItem";
+            this.загрузитьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.загрузитьToolStripMenuItem.Text = "Загрузить";
+            this.загрузитьToolStripMenuItem.Click += new System.EventHandler(this.загрузитьToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(280, 111);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Нарисовать граф";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 661);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа по ТПР №1, Асадуллин А.Д., Данилов Р.А., МО-407";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TPR1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            bm = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+
+        int n; //кол-во состояний
+        int k; //кол-во стратегий
+        int iter; //кол-во итераций
+        static int max = 6;
+        Bitmap bm;
+        
+        double[,,] p = new double[max, max, max];
+        double[,,] r = new double[max, max, max];
+
+        double[,] q = new double[max, max];
+
+        double[,] v = new double[max, max+1];
+        int[,] d = new int[max, max+1];
+
+        private void init()
+        {
+            n = Convert.ToInt32(textBox1.Text);
+            k = Convert.ToInt32(textBox2.Text);
+            iter = Convert.ToInt32(textBox3.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            showMatrix();
+        }
+
+        void fillMatrix()
+        {
+            for (int i = 0; i < n; i++)
+                for (int j =0; j< n; j++)
+                    for (int l = 0; l < k; l++)
+                    p[i, j, l] = Convert.ToDouble(dataGridView1.Rows[(i * k)+l].Cells[j + 2].Value);
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    for (int l = 0; l < k; l++)
+                        r[i, j, l] = Convert.ToDouble(dataGridView1.Rows[(i * k) + l].Cells[j + n + 2].Value);
+        }
+
+        void showMatrix()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            init();
+
+            dataGridView1.Columns.Add("N", "Состояния");
+            dataGridView1.Columns.Add("K", "Стратегии");
+            for (int i = 0; i < n; i++)
+            {
+                dataGridView1.Columns.Add((i + 1).ToString(), "Вероятность состояния " + (i + 1).ToString());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                dataGridView1.Columns.Add((i + 1).ToString(), "Доходность состояния " + (i + 1).ToString());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < k; j++)
+                    dataGridView1.Rows.Add((i + 1).ToString());
+            }
+            for (int i = 0; i < n * k; i++)
+                dataGridView1.Rows[i].Cells[1].Value = ((i % k + 1).ToString());
+
+            dataGridView1.Columns.Add("Dohod", "Ожидаемые доходности");
+        }
+
+        void fillTotals()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                v[i, 0] = 0;
+                d[i, 0] = -1;
+            }
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < k; j++)
+                {
+                    q[i, j] = 0;
+                    for (int l = 0; l < n; l++)
+                        q[i, j] += p[i, l, j] * r[i, l, j];
+                }
+            for (int i = 1; i <= iter; i++)
+            {
+                double[] vi = new double[n];
+                int[] di = new int[n];
+                for (int j = 0; j < n; j++)
+                {
+                    vi[j] = 0;
+                    di[j] = -1;
+                    for (int k1 = 0; k1 < k; k1++)
+                    {
+                        double tmp = q[j, k1];
+                        for (int n1 = 0; n1 < n; n1++)
+                            tmp += p[j, n1, k1] * v[n1, i - 1];
+                        if (vi[j] < tmp)
+                        {
+                            vi[j] = tmp;
+                            di[j] = k1;
+                        }
+                    }
+                    v[j, i] = vi[j];
+                    d[j, i] = di[j];
+                }
+            }
+        }
+
+        void showTotals()
+        {
+            fillTotals();
+
+            dataGridView2.Rows.Clear();
+            dataGridView2.Columns.Clear();
+            dataGridView1.Columns.Remove("Dohod");
+            //init();
+
+            dataGridView1.Columns.Add("Dohod", "Ожидаемые доходности");
+            dataGridView2.Columns.Add("N", "n");
+            for (int i = 0; i < iter + 1; i++)
+            {
+                dataGridView2.Columns.Add((i + 1).ToString(), "Эпоха " + i.ToString());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                dataGridView2.Rows.Add("V" + (i + 1).ToString());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                dataGridView2.Rows.Add("D" + (i + 1).ToString());
+            }
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < iter + 1; j++)
+                    dataGridView2.Rows[i].Cells[j + 1].Value = Math.Round(v[i, j], 2).ToString();
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < iter + 1; j++)
+                    dataGridView2.Rows[i + n].Cells[j + 1].Value = (d[i, j] + 1).ToString();
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < k; j++)
+                    dataGridView1.Rows[j + i * k].Cells[n * 2 + 2].Value = Math.Round(q[i, j], 2).ToString();
+        }
+
+        void showPRMatrix()
+        {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    for (int l = 0; l < k; l++)
+                        dataGridView1.Rows[(i * k) + l].Cells[j + 2].Value = p[i, j, l];
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    for (int l = 0; l < k; l++)
+                        dataGridView1.Rows[(i * k) + l].Cells[j + n + 2].Value = r[i, j, l];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fillMatrix();
+            showMatrix();
+        }
+
+        private void Drawing()
+        {
+            Graphics g = Graphics.FromImage(bm);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            /*g.Graphics.TextRenderingHint = TextRenderingHint.AntiAlias;
+            g.TextRenderingHint = */
+            //g.Clear(pictureBox1.BackColor);
+
+            Font font = new Font("Arial", 8);
+
+            Pen arrowpen = new Pen(Brushes.LightSlateGray);
+            arrowpen.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+
+            g.FillRectangle(Brushes.White, 0, 0, pictureBox1.Width, pictureBox1.Height);
+
+            for (int i = 0; i<n;i++)
+                for (int j=0; j<n; j++)
+                    for(int l=0; l<k; l++)
+                    {
+                        if (p[i, j, l] != 0)
+                        {
+                            g.DrawLine(arrowpen, 10*2, 100 * i + 37, 550+10, 100 * j + 37);
+                            //g.RotateTransform(10 * (i- j));
+                            //g.DrawString((i + 1).ToString(), font, Brushes.Black, 50+l*20, 100 * i + 27);
+                        }
+                    }
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    for (int l = 0; l < k; l++)
+                    {
+                        if (p[i, j, l] != 0)
+                        {
+                        //g.DrawLine(arrowpen, 10 * 2, 100 * i + 37, 550 + 10, 100 * j + 37);
+                        //g.RotateTransform(10 * (i- j));
+                        g.DrawString(((l + 1) + ": " + p[i,j,l] + "; " + r[i,j,l]).ToString(), font, Brushes.Black, 100 + 80 * l, 100 * i + 30 + (j - i) * 15/*(20-(j-i)*10) + 100 * j- 100 * i +20*l+5*(j-i)  100 * i + 27 + (j-i)*11+ l*5   j*10 + (l-j)*20  + 100*j-100*i*/);
+                            if (p[i, j, l] != 0)
+                                g.DrawString(((i + 1) + " -> " + (j + 1) + ": ").ToString(), font, Brushes.Black, 60, 100 * i + 30 + (j - i) * 15);
+                        }
+                    }
+                    
+                }
+
+            for (int i = 0; i < n; i++)
+            {
+                g.FillEllipse(Brushes.Aqua, 10, 100 * i + 20, 30, 30);
+                g.DrawEllipse(arrowpen, 10, 100 * i + 20, 30, 30);
+                g.DrawString((i + 1).ToString(), font, Brushes.Black, 10*2, 100 * i + 27);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                g.FillEllipse(Brushes.Aqua, 550, 100 * i + 20, 30, 30);
+                g.DrawEllipse(arrowpen, 550, 100 * i + 20, 30, 30);
+                g.DrawString((i + 1).ToString(), font, Brushes.Black, 550+10, 100 * i + 27);
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImageUnscaled(bm, 0, 0);
+        }
+
+        public void Save(StreamWriter stream)
+        {
+            stream.WriteLine(n);
+            stream.WriteLine(k);
+            stream.WriteLine(iter);
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int l = 0; l < k; l++)
+                    {
+                        stream.WriteLine(p[i, j, l].ToString() + " ");
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int l = 0; l < k; l++)
+                    {
+                        stream.WriteLine(r[i, j, l].ToString() + " ");
+                    }
+                }
+            }
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Файлы для марковских процессов|*.tpr";
+            saveFileDialog1.DefaultExt = "tpr";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter stream = new StreamWriter(saveFileDialog1.FileName);
+                Save(stream);
+                stream.Close();
+            }
+        }
+
+        public void LoadFile(StreamReader Stream)
+        {
+            int n = Convert.ToInt32(Stream.ReadLine());
+            textBox1.Text = n.ToString();
+            int k = Convert.ToInt32(Stream.ReadLine());
+            textBox2.Text = k.ToString();
+            int iter = Convert.ToInt32(Stream.ReadLine());
+            textBox3.Text = iter.ToString();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int l = 0; l < k; l++)
+                    {
+                        p[i,j,l] = Convert.ToInt32(Stream.ReadLine());
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int l = 0; l < k; l++)
+                    {
+                        r[i, j, l] = Convert.ToInt32(Stream.ReadLine());
+                    }
+                }
+            }
+
+            showMatrix();
+            showPRMatrix();
+            showTotals();
+            pictureBox1.Image = null;
+            Drawing();
+            pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+        }
+
+        private void загрузитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Файлы для марковских процессов|*.tpr";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader stream = new StreamReader(openFileDialog1.FileName);
+                LoadFile(stream);
+                stream.Close();
+            }
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+
+            Drawing();
+
+            pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+        }
+    }
+}
